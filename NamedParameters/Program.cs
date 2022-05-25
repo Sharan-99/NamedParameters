b947fb654b7ac42578cda3966e5e8cdd5a7760d7
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 11:56:51 +0530

    first commit

---
+++ NamedParameters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    class Program
+    {
+        private static void GetCohortDetails(string cohortName,int count,string track,string mode,string currentModule)
+        {
+            Console.WriteLine("It is {0} with {1} GenCs undergoing training for {2} thru {3}. The current module of training is {4}",
+                              cohortName, count, track, mode, currentModule);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            //Normal Method Call
+            GetCohortDetails("ADM21DF015", 21, "DotNet", "OBL", "MVC");
+
+            //Method call with named parameters in the order
+            GetCohortDetails(cohortName: "ADM21DF015",count:21,track:"DotNet",mode:"OBL",currentModule:"MVC");
+
+            //Method call with named parameters not in the order
+            GetCohortDetails(mode: "OBL", currentModule: "MVC",count: 21,cohortName: "ADM21DF015", track: "DotNet");
+
+            //Method call with incorrect order and without named paramaters
+            GetCohortDetails("DotNet", 21, "OBL", "MVC", "ADM21DF015");
+
+            Console.ReadKey();
+        }
+    }
+}
